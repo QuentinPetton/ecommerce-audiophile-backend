f0ebc942ca902b0c279ef342c9bf2fe397af3f15
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-31 15:24:36 +0200

    feat: add productRepository

---
+++ src/main/java/com/quentinpetton/ecommerce_audiophile_backend/repository/ProductOtherRepository.java
@@ -0,0 +1,7 @@
+package com.quentinpetton.ecommerce_audiophile_backend.repository;
+
+import com.quentinpetton.ecommerce_audiophile_backend.entity.ProductOther;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductOtherRepository extends JpaRepository<ProductOther, Long> {
+}
