2a188d956532aab68365399040120d995e176064
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-31 12:08:05 +0200

    first commit

---
+++ src/main/java/com/quentinpetton/ecommerce_audiophile_backend/EcommerceAudiophileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quentinpetton.ecommerce_audiophile_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAudiophileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAudiophileBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quentinpetton/ecommerce_audiophile_backend/EcommerceAudiophileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quentinpetton.ecommerce_audiophile_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAudiophileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
